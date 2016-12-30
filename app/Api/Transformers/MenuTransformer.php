06aef38f3c64e80e94182ae37b246f607ea24805
陈小龙 <13480731740@qq.com>
2016-12-30 22:52:46 +0800

    Dingo API 初探

---
+++ app/Api/Transformers/MenuTransformer.php
@@ -0,0 +1,13 @@
+<?php
+namespace App\Api\Transformers;
+use App\Menu;
+use League\Fractal\TransformerAbstract;
+
+class MenuTransformer extends TransformerAbstract{
+    public function transform()
+    {
+        return [
+
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,24 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//************API路由
+
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+    $api->group(['namespace' => 'App\Api\Controllers'], function ($api) {
+        //用户登录
+        $api->post('user/login','AuthController@authenticate');
+        //用户注册
+        $api->post('user/register','AuthController@register');
+        //已被中间件保护过滤
+        $api->group(['middleware' => 'jwt.auth'], function ($api) {
+            //获取用户，传递Token值
+            $api->get('user/me','AuthController@getAuthenticatedUser');
+            //显示信息
+            $api->get('lesson','LessonsController@index');
+            //显示信息带ID并且，传递Token值
+            $api->get('lesson/{id}','LessonsController@show');
+        });
+    });
+});
